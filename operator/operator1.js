bc77cd44dde680f76686dca8939d134bf5e555b2
Felix Edna Santosa <felixsantosa123@gmail.com>
2025-04-04 15:41:46 +0700

    Update operator1.js

--- operator/operator1.js
+++ operator/operator1.js
@@ -1,1 +1,7 @@
-
+// Operator di javascripts ada 7 dan dibedakan berdasarkan jumlah operand nya
+// operator aritmatika, penugasan, perbandingan, logika, dan string masuk ke binary
+// sedangkan operator string, kondisional, dan typeof masuk ke ternary
+// operator binary membutuhkan 2 operand misal operator1 operand operator2
+// operator unary membutuhkan 1 operand misal operator operand
+// operator ternary membutuhkan 3 operand 
+// operator aritmatika ada +,-,*,/,%(modulus atau sisa bagi)
