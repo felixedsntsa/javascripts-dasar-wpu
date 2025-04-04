21ca014f380143c0a9ff1bd62d3207dc329ace7d
Felix Edna Santosa <felixsantosa123@gmail.com>
2025-04-04 21:52:36 +0700

    Update variable.js

--- variable/variable.js
+++ variable/variable.js
@@ -1,2 +1,43 @@
 // VARIABLE
-// 
+// sebuah tempat/wadah yang memiliki anam, yang digunakan untuk menyimpan sebuah nilai
+// nilai yang dapat ditampung bisa bermacam-macam bisa number, string, boolean
+
+// DEKLARASI
+// sebuah tahap untuk mendaftarkan variabel ke dalam lingkup yang sesuai
+// INISIALISASI
+// menyediakan memori untuk variabel
+// ASSIGNMENT
+// menetapkan nilai yang spesifik ke dalam variabel
+
+var x; // deklarasi sekaligus inisialisasi variabel bernama x
+x = 20; // assignment (menugaskan angka 20 ke dalam variabel x)
+
+// DEKLARASI VARIABEL
+// var, let, const
+
+// sintaks menuliskan variabel
+// var <nama variabel>;
+// var tidak boleh menggunakan spasi
+// var boleh dengan angka tetapi tidak boleh berada didepan misal (var 1boleh;)
+// bisa var boleh1;
+// bisa var ini_juga_boleh;
+// bisa var $_$;
+
+// FORMAT YANG SERING DIAPAKAI
+// var namaVariabelPanjang; (camel case)
+
+// SHORTHAND
+var nama;
+var umur;
+var lulus;
+
+nama = "Felix";
+umur = 20;
+lulus = true;
+
+// BISA PAKAI SEPERTI DIBAWAH INI
+var nama = "Felix",
+    umur = 20,      
+    lulus = true;
+
+// VARIABLE SCOPE
