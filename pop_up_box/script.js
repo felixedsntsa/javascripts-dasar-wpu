0914bff92b7f37463d59f162013d1b1319cad5b5
Felix Edna Santosa <felixsantosa123@gmail.com>
2025-04-04 22:34:54 +0700

    Add files via upload

---
+++ pop_up_box/script.js
@@ -0,0 +1,37 @@
+// POP UP BOX atau DIALOG BOX
+// ada 3 yaitu alert, prompt, dan confirm
+
+// ALERT
+// alert('Hello');
+// alert('My name is Felix');
+// alert('Nice to meet you!');
+
+// PROMPT
+// PROMPT('masukkan nama:'); 
+// jadi ada textbox di web browser
+// mengembalikan nilai dari apa yang user inputkan
+// var nama = prompt('masukkan nama:');
+// simpan nilai dari prompt ke sebuah variabel agar bisa digunakan lagi(misal alert)
+// alert(nama);
+// setelah mengisi nama nanti alert akan menuliskan apa yang kita sudah tulis di testbox
+
+// CONFIRM
+// confirm('kamu yakin?');
+// mengembalikan nilai boolean(true or false)
+// var tes = confirm('kamu yakin?'); 
+// simpan nilai pada sebuah variabel
+// alert(tes);
+// jika user mengklik ok maka alert yang ditampilkan true sebaliknya jika cancel maka false
+
+// contoh sederhana
+// alert('Selamat Datang!');
+// var lagi = true;
+
+// while(lagi) {
+//     var nama = prompt('masukkan nama:');
+//     alert('halo ' + nama);
+
+//     lagi = confirm('coba lagi?');
+// }
+
+// alert('terima kasih')
