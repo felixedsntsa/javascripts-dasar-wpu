389ed0e7cc61655c31163fde9a601eefc53530d9
Felix Edna Santosa <felixsantosa123@gmail.com>
2025-04-05 13:26:43 +0700

    Add files via upload

---
+++ looping/search_table/script.js
@@ -0,0 +1,16 @@
+// SEARCH TABLE
+
+// var nilaiAwal = 1; // tentukan nilai awal sebelum memulai lopp
+// while(nilaiAwal <= 5) // cek kondisi apakah bernilai true atau false {
+//     console.log('Hello World'); // akan melakukan aksi ketika bernilai true
+//     nilaiAwal++;  // increment nilai awal di akhir   tiap pengulangan
+// }
+
+var jmlAngkot = 10; // bisa dibedakan 
+var noAngkot = 1;   // bisa dibedakan 
+
+while(noAngkot <= jmlAngkot) {
+    console.log('Angkot No. ' + noAngkot + ' beroperasi dengan baik.')
+noAngkot++;
+}
+
