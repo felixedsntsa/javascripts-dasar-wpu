8105b3a36de60861548fd5f3a5f1102a2854e169
Felix Edna Santosa <felixsantosa123@gmail.com>
2025-04-05 13:28:23 +0700

    Add files via upload

---
+++ condition/if_else/script.js
@@ -0,0 +1,32 @@
+// IF ELSE
+
+// SINTAKS
+// if(kondisi) {
+// aksi
+// } else {
+// }
+// akan melakukan aksi jika kondisi bernilai true, jika false akan keluar dari blok
+
+// var angka = 1;
+// if(angka === 1) {
+//     alert('anda memasukkan angka ' + angka)
+// }
+
+// var angka = 2;
+// if(angka === 1) {
+//     alert('anda memasukkan angka ' + angka)
+// }
+// tidak akan dijalankan 
+
+// var angka = 1;
+// if(angka < 5) {
+//     alert(angka + ' lebih kecil dari 5')
+// }
+
+// var angka = prompt('masukkan angka:');
+// if(angka % 2 == 0) {
+//     alert(angka + 'adalah bilangan genap')
+// } else {
+//     alert(angka + 'adalah bilangan ganjil')
+// }
+// jika user menginputkan selain angka maka akan ditangkap di blok else
