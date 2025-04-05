75f33354b3fa990ab546dc24240e16882c20b796
Felix Edna Santosa <felixsantosa123@gmail.com>
2025-04-05 13:23:51 +0700

    Add files via upload

---
+++ looping/for_loop/script.js
@@ -0,0 +1,20 @@
+// FOR LOOP
+
+// SINTAKS WHILE
+// var nilaiAwal = 1;
+// while(nilaiAwal <= 10) {
+//     console.log('hello world');
+// nilaiAwal++;
+// }
+
+// SINTAKS FOR
+// for( nilai awal ; pengkondisian ; increment / decrement ) {
+// }
+
+// for(var nilaiAwal = 1; nilaiAwal <= 10; nilaiAwal++) {
+//     console.log('hello world');
+// }
+
+// for(var nilaiAwal = 20; nilaiAwal > 10; nilaiAwal--) {
+//     console.log('hello world');
+// }
