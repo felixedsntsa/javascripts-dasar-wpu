b3a0f3336c82129d51fcb4adc0c7287d5d98c75f
Felix Edna Santosa <felixsantosa123@gmail.com>
2025-04-06 09:07:47 +0700

    Create tipedatanumber.js

---
+++ tipedata/tipedatanumber.js
@@ -0,0 +1,10 @@
+// dalam javascript tidak ada integer / bilangan bulat, yg ada hanya floating point / pecahan
+// ukuran angka 64 bit yaitu sebesar 2 pangkat 64, namun tidak semua angka tersebut dapat dipakai
+
+/* Angka yg bisa disimpan
+1. angka tanpa desimal -> 10, 1500, 123456. akurat sampai 15 digit
+2. angka dengan desimal -> 3.14, 1.5, 6.876, 100.00. maksimal 17 digit di belakang koma
+3. eksponen -> 123e5 // 12300000, 123e-5 // 0.00123
+4. bilangan negatif -> -25, -0.52
+5. angka spesial -> Infinity I, -Infinity -I, NaN (Not a Number)
+*/
